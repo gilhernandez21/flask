10ccf86af25bec0f395019accced573095b20d74
gilhernandez21 <gilbert.hernandez0914@gmail.com>
2021-07-19 18:36:47 -0500

    adding data from file to df

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
-import os.path
+#import os.path
+import glob
+import os
+import json
 from flask_wtf import FlaskForm
 from wtforms import FileField
 from flask_uploads import configure_uploads, UploadSet,DATA
@@ -19,10 +22,14 @@ class MyForm(FlaskForm):
 @app.route('/', methods = ['GET','POST'])
 def index():
     form = MyForm()
-    
+    filePath = "C:\\Users\\hernang2\\Documents\\Python\\flask_upload\\uploads\\attachments\\"
     if form.validate_on_submit():
         filename = attachments.save(form.file.data)
-        return f'FileName: { filename }'
+        #return f'FileName: { filename }'
+        with open(filePath+str(filename), "r") as f:
+            data = json.load(f)
+
+        return data
     return render_template('index.html',form = form)
 
 
