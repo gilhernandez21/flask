aea3bb6910547c0f98afb68e7d8445ce45ef983a
gilhernandez21 <49378316+gilhernandez21@users.noreply.github.com>
2021-07-20 15:02:11 -0500

    updated app.py
    now redirects and handles form input

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect,url_for
 import json
 from flask_wtf import FlaskForm
 from wtforms import FileField
 from flask_uploads import configure_uploads, UploadSet,DATA
 from flask_bootstrap import Bootstrap
+from help import setData
 
 
 
@@ -18,9 +19,11 @@ configure_uploads(app,attachments)
 class MyForm(FlaskForm):
     file = FileField('file')
 
+data = 0
 
 @app.route('/', methods = ['GET','POST'])
 def index():
+    global data
     form = MyForm()
     filePath = "C:\\Users\\hernang2\\Documents\\Python\\flask_upload\\uploads\\attachments\\"
     if form.validate_on_submit():
@@ -28,16 +31,27 @@ def index():
         #return f'FileName: { filename }'
         with open(filePath+str(filename), "r") as f:
             data = json.load(f)
-
-        return render_template('display.html',data = data)
-    return render_template('index.html',form = form)
+        return redirect(url_for('display'))
+    else:
+        return render_template('index.html',form = form)
 
 @app.route('/display', methods = ['GET','POST'])
 def display():
-    #form = MyForm()
-    #if form.validate_on_submit():
+    displayData = setData(data)
+    show =[]
+   # print(displayData)
+    if request.method == 'POST':
+        req = request.form['val']
+        for sets in displayData:
+            if req == sets[0]:
+                show = sets[1]
+        if len(show) == 0:
+            show = displayData[0][1]
+        return render_template('display.html', data = data,df=show, type = req)
+    else:
+        return render_template('display.html', data = data)
 
-    return render_template('display.html')
+
 
 if __name__ == "__main__":
     app.run()
